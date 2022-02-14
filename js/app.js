0223e46053737c3fc3a1027b10a50d6e267fd854
kiiirtiiii <kirtivats1409@gmail.com>
2022-02-14 18:03:21 +0530

    code update

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ class Inputs {
   constructor() {
     this.budgetBtn = document.getElementById('budget-btn')
     this.expenseBtn = document.getElementById('expense-btn')
+    this.updateExpenseBtn = document.getElementById('update-expense-btn')
 
     this.budget = document.getElementById('budget')
     this.expense = document.getElementById('expense')
@@ -29,10 +30,14 @@ class ShowValues {
 
     this.editExpense = document.querySelector('.edit-expense')
     this.dltExpense = document.querySelector('.dlt-expense')
+
+    this.textArr
+    this.budgetValue
   }
 
   showBudget() {
     this.budget.innerHTML = inputs.budget.value
+    this.budgetValue = +inputs.budget.value
     validation.budget.innerHTML = ''
     inputs.budget.value = ''
   }
@@ -47,38 +52,82 @@ class ShowValues {
 
     clone.className = 'individual-expense'
     clone.id = 'new-expense' + ++this.i
-
-    clone.innerHTML += inputs.expense.value
-    clone.innerHTML += ' ' + this.expenseArr.at(-1)
+    clone.querySelector('p').innerHTML =
+      inputs.expense.value + ' ' + this.expenseArr.at(-1)
 
     this.original.parentNode.appendChild(clone)
-    console.log(clone)
-    console.log(this.expenseArr)
+
+    inputs.expense.value = ''
+    inputs.expenseAmount.value = ''
+  }
+
+  update(expenseValue) {
+    validation.expense.innerHTML = ''
+    validation.expenseAmount.innerHTML = ''
+
+    this.textArr = []
+    this.textArr.push(inputs.expense.value)
+    this.textArr.push(inputs.expenseAmount.value)
+
+    expenseValue.innerHTML = this.textArr.join(' ')
+    this.expenseArr.push(+inputs.expenseAmount.value)
 
     inputs.expense.value = ''
     inputs.expenseAmount.value = ''
 
-    if (inputs.budgetBtn.innerHTML == 'Update Expense') {
-      this.delete()
-    }
+    inputs.expenseBtn.style.display = 'block'
+    inputs.updateExpenseBtn.style.display = 'none'
   }
 
   edit(e) {
-    let parentNodetext = e.parentNode.innerText
-    let textArr = parentNodetext.split(' ')
-    console.log(textArr)
+    let expenseValue = e.parentNode.querySelector('p')
 
-    inputs.expense.value = textArr.slice(2, -1, -1).join(' ')
-    inputs.expenseAmount.value = textArr.at(-1)
+    this.removeExpense(e)
+    inputs.expense.value = this.textArr.slice(0, -1, -1).join(' ')
+    inputs.expenseAmount.value = this.textArr.at(-1)
 
-    let idx = this.expenseArr.findIndex((p) => p == inputs.expenseAmount.value)
-    console.log(idx)
+    inputs.expenseBtn.style.display = 'none'
+    inputs.updateExpenseBtn.style.display = 'block'
 
-    inputs.expenseBtn.innerHTML = 'Update Expense'
+    inputs.updateExpenseBtn.addEventListener('click', function () {
+      if (inputs.expense.value == null || inputs.expense.value == '') {
+        validation.expense.innerHTML = 'Expense can not be empty'
+      } else if (
+        inputs.expenseAmount.value == null ||
+        inputs.expenseAmount.value == ''
+      ) {
+        validation.expense.innerHTML = ''
+        validation.expenseAmount.innerHTML = 'Expense Amount can not be empty'
+      } else {
+        showValues.update(expenseValue)
+        showValues.showSavings()
+      }
+    })
+  }
+
+  removeExpense(e) {
+    let expenseValue = e.parentNode.querySelector('p')
+    this.textArr = expenseValue.innerHTML.split(' ')
+
+    let idx = this.expenseArr.findIndex((p) => p == this.textArr.at(-1))
+    this.expenseArr.splice(idx, 1)
   }
 
   delete(e) {
     document.getElementById(e.parentNode.id).remove()
+    showValues.removeExpense(e)
+    showValues.showSavings()
+  }
+
+  showSavings() {
+    if (this.expenseArr.length == 0) {
+      this.savings.innerHTML = this.budgetValue
+    } else {
+      let totalExpense = this.expenseArr.reduce((x, y) => {
+        return x + y
+      }, 0)
+      this.savings.innerHTML = this.budgetValue - totalExpense
+    }
   }
 }
 
@@ -91,6 +140,7 @@ inputs.budgetBtn.addEventListener('click', function () {
     validation.budget.innerHTML = 'Please enter your budget'
   } else {
     showValues.showBudget()
+    showValues.showSavings()
   }
 })
 
@@ -106,11 +156,6 @@ inputs.expenseBtn.addEventListener('click', function () {
       'Please enter the amount of your expense'
   } else {
     showValues.showExpense()
+    showValues.showSavings()
   }
 })
-
-// showValues.editExpense.addEventListener('click', function () {
-//   inputs.expense.value = 'kirti'
-// })
-
-// console.log(showValues.editExpense)
